2f3f2140b9c44d1f6000a9c0e191c516a25192c8
nikkosz <ghostsoldier99@gmail.com>
2023-07-16 00:57:09 +0200

    Added 2 buttons and 2 action listeners... More to come Just couple minutes of work done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.Color.*;
 
 public class Main extends JFrame {
 
     private JPanel Main_Panel;
+    private JPanel LEFT_BAR;
+    public JButton Initialize;
+
+    public JButton quit;
 
 
+    public Main() {
+        Initialize.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                initialize();
+            }
+        });
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    private void initialize() {
+
+    }
 
     public static void main(String[] args){
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setContentPane(new Main().Main_Panel);
+        frame.setTitle("Swing_learning");
         frame.pack();
         frame.setVisible(true);
 
